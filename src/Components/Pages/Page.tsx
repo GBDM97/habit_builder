23e737c1a20ed76f32322e9fbde54d61c4e96a84
GBDM97 <ghian.m@gmail.com>
2022-09-09 20:51:46 -0300

    UI improvements

--- src/Components/Pages/Page.tsx
+++ src/Components/Pages/Page.tsx
@@ -20,7 +20,7 @@ function Page() {
     const [currentV, setV] = React.useState("");
     const [currentN, setN] = React.useState(0);
     const [currentM, setM] = React.useState("");
-    const [currentTask, setTask] = React.useState("Pages you will read on this day:");
+    const [currentTask, setTask] = React.useState("Daily pages you will read on this month:");
     const [currentFirst, setFirst] = React.useState(false);
     const [currentSource, setSource] = React.useState("");
     const [currentMeasure, setMeasure] = React.useState("");
@@ -32,6 +32,11 @@ function Page() {
     const [current4, set4] = React.useState(0);
     const [current5, set5] = React.useState(0);
     const [current6, set6] = React.useState(0);
+    const [current7, set7] = React.useState(0);
+    const [current8, set8] = React.useState(0);
+    const [current9, set9] = React.useState(0);
+    const [current10, set10] = React.useState(0);
+
     
             const opt1 = "Reading"
             const opt2 = "Exercising"
@@ -39,15 +44,15 @@ function Page() {
 
             const handleChange = (e:Page) => {
                 
-                if(e.target.value === "Reading"){setTask("Pages you will read on this day:"); setV("read"); 
+                if(e.target.value === "Reading"){setTask("Daily pages you will read on this month:"); setV("read"); 
                 setSource("https://cdn.pixabay.com/photo/2016/11/18/16/56/book-1835799_960_720.jpg"); 
                 setMeasure("pages");}
 
-                if(e.target.value === "Exercising"){setTask("Minutes you will exercise on this day:"); setV("exercise");
+                if(e.target.value === "Exercising"){setTask("Daily minutes you will exercise on this month:"); setV("exercise");
                 setSource("https://cdn.pixabay.com/photo/2021/05/14/08/44/running-6252827_960_720.jpg");
                 setMeasure("hours");}
 
-                if(e.target.value === "Studying"){setTask("Minutes you will study on this day:"); setV("study"); 
+                if(e.target.value === "Studying"){setTask("Daily minutes you will study on this month:"); setV("study"); 
                 setSource("https://cdn.pixabay.com/photo/2017/08/01/00/38/man-2562325_960_720.jpg");
                 setMeasure("hours");}
 
@@ -64,13 +69,17 @@ function Page() {
                 setN(e.target.value);
 
                 if (currentV === "study") {
-                if (e.target.value >= 120) {set0(120)} else {set0(e.target.value)}
+                set0(e.target.value)
                 if (e.target.value * 2 >= 120) {set1(120)} else {set1(e.target.value * 2)}
                 if (e.target.value * 3 >= 120) {set2(120)} else {set2(e.target.value * 3)}
                 if (e.target.value * 4 >= 120) {set3(120)} else {set3(e.target.value * 4)}
                 if (e.target.value * 5 >= 120) {set4(120)} else {set4(e.target.value * 5)}
                 if (e.target.value * 6 >= 120) {set5(120)} else {set5(e.target.value * 6)}
                 if (e.target.value * 7 >= 120) {set6(120)} else {set6(e.target.value * 7)}
+                if (e.target.value * 8 >= 120) {set7(120)} else {set7(e.target.value * 8)}
+                if (e.target.value * 9 >= 120) {set8(120)} else {set8(e.target.value * 9)}
+                if (e.target.value * 10 >= 120) {set9(120)} else {set9(e.target.value * 10)}
+                if (e.target.value * 11 >= 120) {set10(120)} else {set10(e.target.value * 11)}
                 }
 
                 if (currentV === "exercise") {
@@ -81,16 +90,24 @@ function Page() {
                 if (e.target.value * 5 >= 90) {set4(90)} else {set4(e.target.value * 5)}
                 if (e.target.value * 6 >= 90) {set5(90)} else {set5(e.target.value * 6)}
                 if (e.target.value * 7 >= 90) {set6(90)} else {set6(e.target.value * 7)}
+                if (e.target.value * 8 >= 90) {set7(90)} else {set7(e.target.value * 8)}
+                if (e.target.value * 9 >= 90) {set8(90)} else {set8(e.target.value * 9)}
+                if (e.target.value * 10 >= 90) {set9(90)} else {set9(e.target.value * 10)}
+                if (e.target.value * 11 >= 90) {set10(90)} else {set10(e.target.value * 11)}
                 }
 
                 if (currentV === "read") {
-                set0(e.target.value)
-                set1(e.target.value * 2)
-                set2(e.target.value * 3)
-                set3(e.target.value * 4)
-                set4(e.target.value * 5)
-                set5(e.target.value * 6)
-                set6(e.target.value * 7)
+                if (e.target.value >= 500) {set0(500)} else {set0(e.target.value)}
+                if (e.target.value * 2 >= 500) {set1(500)} else {set1(e.target.value * 2)}
+                if (e.target.value * 3 >= 500) {set2(500)} else {set2(e.target.value * 3)}
+                if (e.target.value * 4 >= 500) {set3(500)} else {set3(e.target.value * 4)}
+                if (e.target.value * 5 >= 500) {set4(500)} else {set4(e.target.value * 5)}
+                if (e.target.value * 6 >= 500) {set5(500)} else {set5(e.target.value * 6)}
+                if (e.target.value * 7 >= 500) {set6(500)} else {set6(e.target.value * 7)}
+                if (e.target.value * 8 >= 500) {set7(500)} else {set7(e.target.value * 8)}
+                if (e.target.value * 9 >= 500) {set8(500)} else {set8(e.target.value * 9)}
+                if (e.target.value * 10 >= 500) {set9(500)} else {set9(e.target.value * 10)}
+                if (e.target.value * 11 >= 500) {set10(500)} else {set10(e.target.value * 11)}
                 }
 
                 if (currentFirst === false) {setFirst(true); time();}
@@ -165,6 +182,10 @@ function Page() {
                         current4={current4}
                         current5={current5}
                         current6={current6}
+                        current7={current7}
+                        current8={current8}
+                        current9={current9}
+                        current10={current10}
                         currentM={currentM}
                         currentV={currentV}
                         currentSource={currentSource}
--- src/Components/Pages/Results.tsx
+++ src/Components/Pages/Results.tsx
@@ -12,6 +12,10 @@ export type ResultsArgs = {
     current4: number,
     current5: number,
     current6: number,
+    current7: number,
+    current8: number,
+    current9: number,
+    current10: number,
     currentN: number,
     currentM: string, 
     currentMeasure: string, 
@@ -22,7 +26,7 @@ export type ResultsArgs = {
 }
 
     const d = new Date();
-    let day = d.getDay();
+    let day = d.getMonth();
     let today;
     let D0:string;
     let D1:string;
@@ -31,16 +35,27 @@ export type ResultsArgs = {
     let D4:string;
     let D5:string;
     let D6:string;
-    if (day === 0) {today = "S"; D0 = "M"; D1 = "T"; D2 = "W"; D3 = "T"; D4 = "F"; D5 = "S"; D6 = "S";}
-    if (day === 1) {today = "M"; D0 = "T"; D1 = "W"; D2 = "T"; D3 = "F"; D4 = "S"; D5 = "S"; D6 = "M";}
-    if (day === 2) {today = "T"; D0 = "W"; D1 = "T"; D2 = "F"; D3 = "S"; D4 = "S"; D5 = "M"; D6 = "T";}
-    if (day === 3) {today = "W"; D0 = "T"; D1 = "F"; D2 = "S"; D3 = "S"; D4 = "M"; D5 = "T"; D6 = "W";}
-    if (day === 4) {today = "T"; D0 = "F"; D1 = "S"; D2 = "S"; D3 = "M"; D4 = "T"; D5 = "W"; D6 = "T";}
-    if (day === 5) {today = "F"; D0 = "S"; D1 = "S"; D2 = "M"; D3 = "T"; D4 = "W"; D5 = "T"; D6 = "F";}
-    if (day === 6) {today = "S"; D0 = "S"; D1 = "M"; D2 = "T"; D3 = "W"; D4 = "T"; D5 = "F"; D6 = "S";}
+    let D7:string;
+    let D8:string;
+    let D9:string;
+    let D10:string;
+    
+    if (day === 0) {today = "JAN"; D0 = "FEB"; D1 = "MAR"; D2 = "APR"; D3 = "MAY"; D4 = "JUNE"; D5 = "JULY"; D6 = "AUG"; D7 = "SEPT"; D8 = "OCT"; D9 = "NOV"; D10 = "DEC";}
+    if (day === 1) {today = "FEB"; D0 = "MAR"; D1 = "APR"; D2 = "MAY"; D3 = "JUNE"; D4 = "JULY"; D5 = "AUG"; D6 = "SEPT"; D7 = "OCT"; D8 = "NOV"; D9 = "DEC"; D10 = "JAN";}
+    if (day === 2) {today = "MAR"; D0 = "APR"; D1 = "MAY"; D2 = "JUNE"; D3 = "JULY"; D4 = "AUG"; D5 = "SEPT"; D6 = "OCT"; D7 = "NOV"; D8 = "DEC"; D9 = "JAN"; D10 = "FEB";}
+    if (day === 3) {today = "APR"; D0 = "MAY"; D1 = "JUNE"; D2 = "JULY"; D3 = "AUG"; D4 = "SEPT"; D5 = "OCT"; D6 = "NOV"; D7 = "DEC"; D8 = "JAN"; D9 = "FEB"; D10 = "MAR";}
+    if (day === 4) {today = "MAY"; D0 = "JUNE"; D1 = "JULY"; D2 = "AUG"; D3 = "SEPT"; D4 = "OCT"; D5 = "NOV"; D6 = "DEC"; D7 = "JAN"; D8 = "FEB"; D9 = "MAR"; D10 = "APR";}
+    if (day === 5) {today = "JUNE"; D0 = "JULY"; D1 = "AUG"; D2 = "SEPT"; D3 = "OCT"; D4 = "NOV"; D5 = "DEC"; D6 = "JAN"; D7 = "FEB"; D8 = "MAR"; D9 = "APR"; D10 = "MAY";}
+    if (day === 6) {today = "JULY"; D0 = "AUG"; D1 = "SEPT"; D2 = "OCT"; D3 = "NOV"; D4 = "DEC"; D5 = "JAN"; D6 = "FEB"; D7 = "MAR"; D8 = "APR"; D9 = "MAY"; D10 = "JUNE";}
+    if (day === 7) {today = "AUG"; D0 = "SEPT"; D1 = "OCT"; D2 = "NOV"; D3 = "DEC"; D4 = "JAN"; D5 = "FEB"; D6 = "MAR"; D7 = "APR"; D8 = "MAY"; D9 = "JUNE"; D10 = "JULY";}
+    if (day === 8) {today = "SEPT"; D0 = "OCT"; D1 = "NOV"; D2 = "DEC"; D3 = "JAN"; D4 = "FEB"; D5 = "MAR"; D6 = "APR"; D7 = "MAY"; D8 = "JUNE"; D9 = "JULY"; D10 = "AUG";}
+    if (day === 9) {today = "OCT"; D0 = "NOV"; D1 = "DEC"; D2 = "JAN"; D3 = "FEB"; D4 = "MAR"; D5 = "APR"; D6 = "MAY"; D7 = "JUNE"; D8 = "JULY"; D9 = "AUG"; D10 = "SEPT";}
+    if (day === 10) {today = "NOV"; D0 = "DEC"; D1 = "JAN"; D2 = "FEB"; D3 = "MAR"; D4 = "APR"; D5 = "MAY"; D6 = "JUNE"; D7 = "JULY"; D8 = "AUG"; D9 = "SEPT"; D10 = "OCT";}
+    if (day === 11) {today = "DEC"; D0 = "JAN"; D1 = "FEB"; D2 = "MAR"; D3 = "APR"; D4 = "MAY"; D5 = "JUNE"; D6 = "JULY"; D7 = "AUG"; D8 = "SEPT"; D9 = "OCT"; D10 = "DEC";}
+
 
 const Results = ({currentTask,
-    current0, current1, current2, current3, current4, current5, current6, currentM, currentMeasure, currentV, currentSource, cToInit, hidden}:ResultsArgs) => {
+    current0, current1, current2, current3, current4, current5, current6, current7, current8, current9, current10, currentM, currentMeasure, currentV, currentSource, cToInit, hidden}:ResultsArgs) => {
 
     if (hidden) return null;
     else return(
@@ -95,6 +110,30 @@ const Results = ({currentTask,
                                         <p className="smallP">{currentTask}&nbsp;</p><p className="number">{current6}</p>
                                     </div>
                                 </div>
+                                <div className="weekContainer">
+                                    <p className='titleW'>{D7}</p>
+                                    <div className="rowContainer">
+                                        <p className="smallP">{currentTask}&nbsp;</p><p className="number">{current7}</p>
+                                    </div>
+                                </div>
+                                <div className="weekContainer">
+                                    <p className='titleW'>{D8}</p>
+                                    <div className="rowContainer">
+                                        <p className="smallP">{currentTask}&nbsp;</p><p className="number">{current8}</p>
+                                    </div>
+                                </div>
+                                <div className="weekContainer">
+                                    <p className='titleW'>{D9}</p>
+                                    <div className="rowContainer">
+                                        <p className="smallP">{currentTask}&nbsp;</p><p className="number">{current9}</p>
+                                    </div>
+                                </div>
+                                <div className="weekContainer">
+                                    <p className='titleW'>{D10}</p>
+                                    <div className="rowContainer">
+                                        <p className="smallP">{currentTask}&nbsp;</p><p className="number">{current10}</p>
+                                    </div>
+                                </div>
                         </div> 
                     
                     
@@ -109,7 +148,7 @@ const Results = ({currentTask,
                     <p className="titleOut">The earlier you start the better</p>
                     <p className="smallP">If you start following this plan for at least
                     one month, at the end of it you will {currentV}:</p>
-                    <p className="motiv">{Math.ceil(currentMeasure === "hours" ? current6 * 31 / 60 : current6 * 31)} {currentMeasure}</p>
+                    <p className="motiv">{Math.ceil(currentMeasure === "hours" ? current0 * 31 / 60 : current0 * 31)} {currentMeasure}</p>
 
                     <img className="Img space" src={currentSource}
                     alt="Chosen habit"></img>
